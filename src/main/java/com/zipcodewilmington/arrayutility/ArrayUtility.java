e47a19b8569008d66f2842470208a093f1f1c0c7
Ashley Smith <ashleysmith@zipcoders-MBP.fios-router.home>
2019-03-17 23:47:34 -0400

    all test are passing

--- src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
+++ src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
@@ -1,7 +1,68 @@
 package com.zipcodewilmington.arrayutility;
 
+import java.util.Arrays;
 /**
  * Created by leon on 3/6/18.
  */
-public class ArrayUtility {
-}
+public class ArrayUtility <T>{
+
+        T[] array;
+
+        public ArrayUtility(T[] array) {
+            this.array = array;
+        }
+
+        public Integer countDuplicatesInMerge(T[] arraysToMerge, T valueToEvaluate) {
+            array = mergeArrays(arraysToMerge);
+            return getNumberOfOccurrences(valueToEvaluate);
+        }
+
+        public T getMostCommonFromMerge(T[] arrayToMerge) {
+            T[] mergedArray = mergeArrays(arrayToMerge);
+            int max = 0;
+            for(int i = 1; i <  mergedArray.length; i++){
+                if (countDuplicatesInMerge(arrayToMerge, mergedArray[i]) >
+                        (countDuplicatesInMerge(arrayToMerge, mergedArray[max]))){
+                    max = i;
+                }
+            }
+            return mergedArray[max];
+        }
+
+        public Integer getNumberOfOccurrences(T object) {
+
+            Integer count = 0;
+            for (T element : array) {
+                if (element == object) {
+
+                    count++;
+                }
+            }
+            return count;
+        }
+
+        public T[] removeValue(T object) {
+            Integer j = 0;
+            T[] result = Arrays.copyOf(array,array.length - getNumberOfOccurrences(object));
+            for (int i = 0; i < array.length; i++) {
+                if(array[i] != object){
+                    result[j] = array[i];
+                    j++;
+                }
+            }
+            return result;
+        }
+
+        public T[] mergeArrays(T[] arrayToMerge) {
+            T[] result = Arrays.copyOf(array, array.length + arrayToMerge.length);
+            for (int i = 0; i < array.length ; i++) {
+                result[i] = array[i];
+            }
+            for (int i = array.length, j = 0; i < result.length; i++, j++) {
+                result[i] = arrayToMerge[j];
+            }
+            return result;
+        }
+    }
+
+
